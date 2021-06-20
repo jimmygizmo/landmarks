34ee44fbbfc98cded1b7f992e5c627a2bebdb474
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2021-06-20 14:32:21 -0700

    created fresh generic xcode ios app project. Interface - SwiftUI, Life Cycle - SwiftUI App, Language - Swift. adjusted gitignore and added required generic apple license from tutorial content. My own license (GNU GPL 3.0) was added by GitHub.com where the repo was created. This is a fresh, generic project, unmodified after Xcode template creation.

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jimmy Gizmo on 6/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jimmy Gizmo on 6/20/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
